3fceec1c2e1380d4d566a372ede26d9c973f4edc
Raúl Gallego Montes <rgallego@futurespace.es>
2024-05-10 01:20:56 +0200

    Primer Commit .2

--- JuegoAhorcado/main.py
+++
@@ -1,7 +0,0 @@
-from Hangman import Hangman
-
-
-juego = Hangman()
-juego.load()
-juego.get_number_of_words()
-juego.playing()
--- PythonYT/helper.py
+++
@@ -1,4 +0,0 @@
-import logging
-logger = logging.getLogger(__name__)
-#logger.propagate=False  (Esto si no se quiere que se propague el formato puesto en basicConfig. Por defecto es True)
-logger.info("hello from helper")
